ab131a156c40c471b83407da24bd2d90f57b0757
Maatla-creator <maatlamog@gmail.com>
2021-09-28 17:36:08 -0700

    add all project files

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+""" Class Square definesa square object"""
+
+
+class Square:
+    """Class Square that defines a square object
+    This class has no public attributes
+    """
+    def __init__(self, size):
+        """Initialize method that stores the size of the square
+        Args:
+             size (int): Defines size of square
+        """
+        self.__size = size
